4715bd3144739c895380619eb371a6c160eb2f63
Gavin <gbd2@rice.edu>
2025-04-15 14:18:11 -0500

    bad, incomplete version of read_data

--- read_data.py
+++ read_data.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def parse_cor_file(file_path):
     """
     Parses a .cor file in SMPS format and returns the model components.
@@ -199,3 +201,57 @@ def parse_sto_second_file(file_path):
                 blocks[period] = []
             blocks[period].append({demandname: demandvalue, 'prob': prob})
     return info, blocks 
+
+def clean_data(cor_file, tim_file, sto_first_file, sto_second_file):
+    """
+    Cleans the data and puts it in the format that is ready
+    for modeling.
+    
+    Inputs:
+        cor_file: path to the .cor file
+        tim_file: path to the .tim file
+        sto_first_file: path to the .sto.first file
+        sto_second_file: path to the .sto.second file
+        
+    Outputs: cost_matrix: cost matrix of the aircraft-to-route represented as a numpy array
+             aircraft_to_hours: aircraft-to-route hours represented as a numpy array
+             capacity_matrix: capacity matrix of the aircraft-to-route represented as a numpy array
+             max_flight_hours: maximum flight hours of each aircraft represented as a numpy array
+             demand_prob_tuple: tuple of demand and probability for each route
+    """
+    # Parse the files
+    cor_data = parse_cor_file(cor_file)
+    sto_first_data = parse_sto_first_file(sto_first_file)
+    sto_second_data = parse_sto_second_file(sto_second_file)
+    cost_matrix = np.array([])
+    aircraft_to_hours = np.array([])
+    capacity_matrix = np.array([])
+    max_flight_hours = np.array([])
+    demand_prob_tuple = []
+    
+    # Extract data from parsed files
+    for colname, i in enumerate(cor_data['columns'].keys()):
+        if len(colname == 3):
+            # We know that the column is X_{ij} (i.e. aircraft i to route j)
+            cost_matrix = np.append(cost_matrix, cor_data['columns'][colname][0][1])
+            capacity_matrix = np.append(capacity_matrix, cor_data['columns'][colname][-1][1])
+            aircraft_to_hours = np.append(aircraft_to_hours, cor_data['columns'][colname][1][1])
+    for colname, i in enumerate(cor_data['rhs'].keys()):
+        if len(colname == 6):
+            # We know that the column is F_i (i.e. maximum flight hours of aircraft i)
+            max_flight_hours = np.append(max_flight_hours, cor_data['rhs'][colname])
+            
+            
+    demand_prob_tuple = []
+    for block, periods in sto_first_data[1].items():
+        for period, demands in periods.items():
+            for demand in demands:
+                demand_prob_tuple.append((demand, demand['prob']))
+
+    return cost_matrix, aircraft_to_hours, capacity_matrix, max_flight_hours, demand_prob_tuple
+
+# print(clean_data('airlift/AIRL.cor', 'airlift/AIRL.tim', 'airlift/AIRL.sto.first', 'airlift/AIRL.sto.second'))
+
+cor_results = parse_cor_file('airlift/AIRL.cor')
+
+print(cor_results['columns'].keys())
